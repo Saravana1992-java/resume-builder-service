2ff5195ced852347ae04c52ce031242251089aef
Saravana manikandan <sarvanamani1992@gmail.com>
2025-05-23 22:53:09 +0530

    Initial project setup with Gradle, Spring Boot, and basic application structure

---
+++ src/test/java/com/saravanan/resume_builder_service/ResumeBuilderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saravanan.resume_builder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
